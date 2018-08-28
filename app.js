73a68ffb8906e9690658340004630882f22d4a9b
joel2035 <joel.pokam@hetic.net>
2018-08-29 01:11:07 +0200

    modif+comment

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 angular.module("Webmail", ["ngSanitize"])
+  // le service location nous permet d'inclure l'etat de l'application dans l'url.
   .controller("WebmailCtrl", function($scope, $location) {
-
     $scope.dossiers = [{
         value: "RECEPTION",
         label: 'Boite de réception',
         emails: [{
             id: 1,
             from: "Albator",
-            to: "Rudy",
+            to: "Joel",
             subject: "Je reviens",
             date: "20/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis."
@@ -15,7 +15,7 @@ angular.module("Webmail", ["ngSanitize"])
           {
             id: 2,
             from: "Capitaine Flam",
-            to: "Rudy",
+            to: "Joel",
             subject: "Bisous de l'espace",
             date: "18/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>."
@@ -23,7 +23,7 @@ angular.module("Webmail", ["ngSanitize"])
           {
             id: 3,
             from: "Pikachu",
-            to: "Rudy",
+            to: "Joel",
             subject: "Pika pika !",
             date: "15/03/2014",
             content: "Pika pika ! Chuuuuuu. Pika pika ! Chuuuuuu. Pika pika ! Chuuuuuu. Pika pika ! Pika pika ? Piiiiika Chuuuuuu. Pika pika ! Pikachu. Pika pika pika."
@@ -31,7 +31,7 @@ angular.module("Webmail", ["ngSanitize"])
           {
             id: 4,
             from: "Barbapapa",
-            to: "Rudy",
+            to: "Joel",
             subject: "Hulahup Barbatruc",
             date: "13/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>."
@@ -44,7 +44,7 @@ angular.module("Webmail", ["ngSanitize"])
         emails: [{
             id: 5,
             from: "Candy",
-            to: "Rudy",
+            to: "Joel",
             subject: "Bon anniversaire",
             date: "20/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis."
@@ -52,7 +52,7 @@ angular.module("Webmail", ["ngSanitize"])
           {
             id: 6,
             from: "Hiro Nakamura",
-            to: "Rudy",
+            to: "Joel",
             subject: "Konichiwa",
             date: "18/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>."
@@ -60,7 +60,7 @@ angular.module("Webmail", ["ngSanitize"])
           {
             id: 7,
             from: "Asuka Langley Soryu",
-            to: "Rudy",
+            to: "Joel",
             subject: "Ca va chier",
             date: "20/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis."
@@ -72,7 +72,7 @@ angular.module("Webmail", ["ngSanitize"])
         label: "Envoyés",
         emails: [{
             id: 8,
-            from: "Rudy",
+            from: "Joel",
             to: "Albator",
             subject: "Bien la famille ?",
             date: "20/03/2014",
@@ -80,7 +80,7 @@ angular.module("Webmail", ["ngSanitize"])
           },
           {
             id: 9,
-            from: "Rudy",
+            from: "Joel",
             to: "Capitaine Flam",
             subject: "Gloubiboulga Night",
             date: "18/03/2014",
@@ -94,7 +94,7 @@ angular.module("Webmail", ["ngSanitize"])
         emails: [{
             id: 10,
             from: "Rue du discount",
-            to: "Rudy",
+            to: "Joel",
             subject: "Envie d'un nouveau frigo ?",
             date: "20/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id ligula ac sem fringilla mattis. Nullam sodales mi vel semper volutpat. Phasellus lorem leo, luctus a lectus id, posuere aliquet orci. Praesent sit amet ipsum porttitor, tempus odio vel, bibendum mauris. Etiam magna lorem, rhoncus eget euismod ac, lobortis quis."
@@ -102,70 +102,80 @@ angular.module("Webmail", ["ngSanitize"])
           {
             id: 11,
             from: "Sofinnoga",
-            to: "Rudy",
+            to: "Joel",
             subject: "Besoin d'argent ?",
             date: "18/03/2014",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent consectetur elementum leo. Curabitur luctus, magna a tempor sodales, orci velit dictum magna, nec pharetra turpis ante vehicula ante. Sed sed libero suscipit, rutrum ligula vel, tempor lorem. Phasellus pulvinar dolor ac velit porttitor pulvinar. Mauris felis quam, consequat at <b>mauris</b>."
           }
         ]
       }
+
     ];
-
-    $scope.dossierCourant = null;
-    $scope.emailSelectionne = null;
-
+    $scope.dossierCourant = null; // on initialiser le dossier courant à null pour commencer
+    $scope.emailSelectionne = null // on initialise le emailSelectionne à null pour commencer
+    // on implemente verEmail dans le controller
     $scope.versEmail = function(dossier, email) {
-      $location.path("/" + dossier.value + "/" + email.id);
+      $location.path("/" + dossier.value + "/" + email.id)
     }
-
     $scope.selectionDossier = function(dossier) {
       $scope.dossierCourant = dossier;
-      $scope.emailSelectionne = null;
-    }
-
+      $scope.emailSelectionne = null; // pour restaurer les ysteme de mail après la selection d'un mail
+    };
     $scope.selectionEmail = function(email) {
       $scope.emailSelectionne = email;
-    };
-    // tri
+    }
+    // on ajoute les functions du tri
+    //on initialise une variable pour le tri
     $scope.champTri = null;
     $scope.triDescendant = false;
-    $scope.triEmails = function(champ) {
+    $scope.triemails = function(champ) {
       if ($scope.champTri == champ) {
-        $scope.triDescendant == !$scope.triDescendant;
+        $scope.triDescendant = !$scope.triDescendant;
       } else {
         $scope.champTri = champ;
         $scope.triDescendant = false;
       }
 
     }
+    // on crè une fonction pour trié les emails
     $scope.cssChevronsTri = function(champ) {
       return {
         glyphicon: $scope.champTri == champ,
         'glyphicon-chevron-up': $scope.champTri == champ && !$scope.triDescendant,
         'glyphicon-chevron-down': $scope.champTri == champ && $scope.triDescendant
-      }
+
+      };
     }
-
-
+    // watch permet de surveiller un function pour voir
     $scope.$watch(function() {
+      // pour acceder au service $location
       return $location.path();
+      // juste après le 2em paramètre de la méthode wath est appeler
     }, function(newPath) {
-      var tabPath = newPath.split("/");
+      var tabPath = newPath.split('/');
+      //testé la longueur du tableau
       if (tabPath.length > 1) {
+        // on recupère la valeur du dossier
         var valDossier = tabPath[1];
+        //on regarde sur chacuns des dossiers pour récuperer le bon
         $scope.dossiers.forEach(function(item) {
           if (item.value == valDossier) {
-            $scope.selectionDossier(item);
+            // on appele la méthode du dossier et on lui passe le dossier en question
+            $scope.selectionDossier(item)
           }
         });
-        if (tabPath.length > 2) {
-          var idMail = tabPath[2];
-          $scope.dossierCourant.emails.forEach(function(item) {
-            if (item.id == idMail) {
-              $scope.selectionEmail(item);
-            }
-          });
-        }
+        // s'il y'a un autre élément dans tabPath alors dans le dossiers il y'a  l'ID du mail
+      }
+      if (tabPath.length > 2) {
+        var idMail = tabPath[2];
+        $scope.dossierCourant.emails.forEach(function(item) {
+          // on verifie si c'est bien le mail que l'on recherche si o
+          if (item.id == idMail) {
+            // si oui alors on affiche notre mails
+            $scope.selectionEmail(item);
+          }
+        })
       }
     });
+
   });
